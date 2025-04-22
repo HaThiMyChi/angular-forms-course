ac4c7fb95b6ec225a02661dddcf2240fcbeb7a60
MyChi <mychikhtn@gmail.com>
2025-04-22 16:53:20 +0700

    Building an Angular Multi-Step Reactive Form with Angular Material

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -19,50 +19,17 @@ interface CourseCategory {
 export class CreateCourseStep1Component implements OnInit {
 
   form = this.fb.group({
-      title: ['', {
-          validators: [
-              Validators.required,
-              Validators.minLength(5),
-              Validators.maxLength(60)
-          ],
-          asyncValidators: [courseTitleValidator(this.courses)],
-          updateOn: 'blur'
-      }],
-      releasedAt: [new Date(), Validators.required],
-      category: ['BEGINNER', Validators.required],
-      downloadsAllowed: [false, Validators.requiredTrue],
-      longDescription: ['', [Validators.required, Validators.minLength(3)]]
-      //address: [null, Validators.required]
+    title: ['', [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(60)
+    ]]
   });
 
-  courseCategories$ : Observable<CourseCategory[]>;
-
-
-  constructor(private fb: FormBuilder, private courses:CoursesService) {
+  constructor(private fb: FormBuilder) {
 
   }
 
   ngOnInit() {
-
-      this.courseCategories$ = this.courses.findCourseCategories();
-
-      const draft = localStorage.getItem("STEP_1");
-
-      if (draft) {
-          this.form.setValue(JSON.parse(draft));
-      }
-
-      this.form.valueChanges
-          .pipe(
-              filter(() => this.form.valid)
-          )
-          .subscribe( val => localStorage.setItem("STEP_1", JSON.stringify(val)));
-
-
   }
-
-  get courseTitle() {
-      return this.form.controls['title'];
-  }
-
 }
