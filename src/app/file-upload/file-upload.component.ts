6de9fe456ef7a0e379077fae92211af9a940061e
MyChi <mychikhtn@gmail.com>
2025-04-23 17:36:40 +0700

    Implementing a File Upload Progress Indicator

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -29,6 +29,7 @@ export class FileUploadComponent {
 
     fileName = '';
     fileUploadError = false;
+    uploadProgress: number;
 
     constructor(private http: HttpClient) {
 
@@ -46,14 +47,24 @@ export class FileUploadComponent {
      
             this.fileUploadError = false;
 
-            this.http.post("http://localhost:9000/api/thumbnail-upload", formData)
-                .pipe(
-                    catchError(error => {
-                        this.fileUploadError = true;
-                        return of(error)
-                    })
-                )
-                .subscribe();
+            this.http.post("http://localhost:9000/api/thumbnail-upload", formData, {
+                reportProgress: true,
+                observe: 'events'
+            })
+            .pipe(
+                catchError(error => {
+                    this.fileUploadError = true;
+                    return of(error)
+                }),
+                finalize(() => {
+                    this.uploadProgress = null;
+                })
+            )
+            .subscribe(event => {
+                if (event.type == HttpEventType.UploadProgress) {
+                    this.uploadProgress = Math.round(100 * (event.loaded / event.total));
+                }
+            });
         }
         
     }
